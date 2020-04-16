3279e5d0dbfebb9540361c125251e032f2f5c40f
Bonface Mudiri <mudirib@gmail.com>
2020-04-16 10:00:37 -0700

    Create User Profile and update models, admin, views, register.html

--- locallibrary/accounts/views.py
+++ locallibrary/accounts/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 # Create your views here.
 def register_view(request):
     """Return the registration template """
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form  = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'register.html', {'form': form })
